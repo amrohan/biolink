ff2c7ca1aeea0657a84385512f4181da379d2a5c
amrohan <rohanxsalunkhe@gmail.com>
2025-01-01 11:59:18 +0530

    add:layout and social links

--- frontend/src/components/UiBuilder.tsx
+++ frontend/src/components/UiBuilder.tsx
@@ -1,5 +1,5 @@
 
-import { PlusOutlined } from "@ant-design/icons";
+import { FacebookOutlined, GithubOutlined, InstagramOutlined, LinkedinOutlined, PlusOutlined, TwitterOutlined } from "@ant-design/icons";
 import { Avatar, Button, Form, Input, Modal } from "antd";
 import { useState } from "react";
 import { LinkCard } from "./LinkCard";
@@ -35,11 +35,42 @@ export type Url = {
 export default function UiBuilder() {
   const { links, setLinks } = useLinks();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSocialOpen, setSocialOpen] = useState(false)
   const [form] = Form.useForm();
+  const [socialForm] = Form.useForm();
   const showModal = () => {
     setIsModalOpen(true);
   };
 
+  const iconLinks = [
+    {
+      title: "Instagram",
+      url: "instagram",
+      icon: <InstagramOutlined />,
+    },
+    {
+      title: "Facebook",
+      url: "facebook",
+      icon: <FacebookOutlined />,
+    },
+    {
+      title: "x(Twitter)",
+      url: "twitter",
+      icon: <TwitterOutlined />,
+    },
+    {
+      title: "Github",
+      url: "github",
+      icon: <GithubOutlined />,
+    },
+    {
+      title: "LinkedIn",
+      url: "linkedin",
+      icon: <LinkedinOutlined />,
+    },
+  ];
+
+  // hnadling ui
   const handleOk = () => {
     form
       .validateFields()
@@ -59,25 +90,49 @@ export default function UiBuilder() {
     form.resetFields();
   };
 
+  const handleSocialOk = () => {
+    socialForm.validateFields()
+      .then((values) => {
+        console.log("Collected Social Links:", values);
+        setSocialOpen(false);
+      })
+      .catch((error) => {
+        console.error("Validation Failed:", error);
+      });
+  };
+
+  const handleSocialCancel = () => {
+    setSocialOpen(false);
+    socialForm.resetFields();
+  };
+
   return (
     <>
       <main className="w-full h-full rounded-md p-4 bg-white border">
-        <div className="h-28 mt-6 flex justify-start items-start w-full">
-          <div className="w-8/12 flex justify-start items-center gap-6">
+        <div className="mi-h-28 max-h-fit mt-6 flex flex-col md:flex-row justify-start items-start w-full">
+          <div className="w-full md:w-11/12 flex flex-col md:flex-row justify-start items-center gap-6">
             <Avatar
               style={{ fontSize: "16px" }}
-              size={{ lg: 64, xl: 80, xxl: 100 }}
-            >
+              size={80} >
               User
             </Avatar>
-            <div className="flex flex-col justify-start items-start">
+            <div className="flex flex-col justify-start items-center md:items-start">
               <h1 className="font-semibold">Rohan Salunkhe</h1>
-              <p className=" text-sm text-neutral-600">A developer</p>
+              <p className=" text-sm  text-neutral-600">Code smarter</p>
+              <div className="flex justify-start items-center gap-2 mt-2">
+                {iconLinks.map((item) => (
+                  <Button icon={item.icon} key={item.title}
+                    onClick={() => setSocialOpen(true)}
+                  >
+                  </Button>
+                ))}
+              </div>
             </div>
           </div>
-          <div className="w-4/12 flex justify-end items-center">
+          <div className="w-full mt-6 md:mt-0 md:w-1/12 flex justify-end items-center">
             <Button
               size="middle"
+              block
               type="primary"
               icon={<PlusOutlined />}
               onClick={showModal}
@@ -128,6 +183,39 @@ export default function UiBuilder() {
           ))}
         </ul>
       </div>
+
+      {/* Model Social Links */}
+      <Modal
+        title="Add Socials"
+        open={isSocialOpen}
+        onOk={handleSocialOk}
+        onCancel={handleSocialCancel}
+      >
+        <Form form={socialForm} layout="vertical">
+          {iconLinks.map((item) => (
+            <Form.Item
+              key={item.url}
+              name={item.url}
+              label={
+                <span className="flex justify-start items-center gap-1.5">
+                  {item.icon} {item.title}
+                </span>
+              }
+              rules={[
+                { required: false, message: `Please enter a ${item.title} URL.` },
+                {
+                  type: "url",
+                  message: "Please enter a valid URL (e.g., https://example.com).",
+                },
+              ]}
+            >
+              <Input placeholder={`Enter your ${item.title} link`} />
+            </Form.Item>
+          ))}
+        </Form>
+      </Modal>
+
+
 
     </>
   );
