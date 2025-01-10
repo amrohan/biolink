843906663359384f3aa4e29d8704e763f9fc85e2
amrohan <rohanxsalunkhe@gmail.com>
2025-01-10 10:52:21 +0530

    add:env for cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import linkRoutes from "./routes/linkRoutes";
-import themeRoute from "./routes/themeRoutes"
+import themeRoute from "./routes/themeRoutes";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Authorization", "Content-Type"],
   }),
